0761ba7f0c20fe352b9b4e25952e52e09d92adee
GabrielRochadeLima <141785217+GabrielRochadeLima@users.noreply.github.com>
2025-08-26 22:07:45 -0300

    Add files via upload

--- AULA 3/exerc3.py
+++ AULA 3/exerc3.py
@@ -1,16 +1,12 @@
-valor_horat = float(input("Digite o valor da hora trabalhada: "))
-hora_trabalho = float(input("Digite as horas trabalhadas no mês: "))
+dias = int(input("Digite a quantidade de dias: "))
+horas = int(input("Digite a quantidade de horas: "))
+minutos = int(input("Digite a quantidade de minutos: "))
+segundos = int(input("Digite a quantidade de segundos: "))
 
-salario_bruto = valor_horat*hora_trabalho
-ir = salario_bruto*0.11
-inss = salario_bruto*0.08
-sindicato = salario_bruto*0.05
+dias_s= dias*24*60*60
+horas_s= horas*60*60
+minutos_s= minutos*60
 
-salario_liquido = salario_bruto - ir - inss - sindicato
+conversao = dias_s+horas_s+minutos_s+segundos
 
-
-print(f"+ Salário bruto: R$ {salario_bruto:.2f}")
-print(f"- IR (11%): R$ {ir:.2f}")
-print(f"- INSS (8%): R$ {inss:.2f}")
-print(f"- Sindicato (5%): R$ {sindicato:.2f}")
-print(f"Seu salário líquido é de: R$ {salario_liquido:.2f}")
+print(f"Tempo total foi de: {conversao} segundos")
