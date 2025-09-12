a6d186b42532dfec6529b23ce54dcb2590b2541f
GabrielRochadeLima <141785217+GabrielRochadeLima@users.noreply.github.com>
2025-09-11 21:24:16 -0300

    Add files via upload

--- AULA 5/exerc5.py
+++ AULA 5/exerc5.py
@@ -3,13 +3,18 @@ codigo = float(input("Digite o código: "))
 
 if(codigo == 1):
     classificacao = "ALimento não perecível"
+    print(classificacao)
 elif(codigo == 2 or codigo == 3 or codigo == 4):
     classificacao = "Alimento perecível"
+    print(classificacao)
 elif(codigo == 5 or codigo == 6):
     classificacao = "Vestuário"
+    print(classificacao)
 elif(codigo == 7):
     classificacao = "Higiene pessoal"
+    print(classificacao)
 elif(codigo >= 8 and codigo <=15):
     classificacao = "Limpeza e utensilios domésticos"
+    print(classificacao)
 else:
     print("Código inválido")
